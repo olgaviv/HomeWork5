21ed5e9654cbddf025fd9ff320ecbd2cdae12a82
olga <olgaviv@mail.ru>
2022-11-14 22:04:54 +0700

    second work

--- HomeWork5/HomeWork5/Program.cs
+++ HomeWork5/HomeWork5/Program.cs
@@ -35,4 +35,45 @@ ShowArray(myArray);
 int evenNumbers = FindEvenNumbers(myArray);
 Console.WriteLine("Количество четных элементов в массиве: "+ evenNumbers);
 */
-// 
+
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+
+int [] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];// инициализация массива
+
+    for (int i = 0; i < size; i++)
+    
+        array[i] = new Random().Next(minValue, maxValue +1);
+    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int FindSum(int[]array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i=i+2)
+    
+         sum+=array[i];
+    return sum;
+    
+}
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите максимальное значение ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [] myArray = CreateRandomArray(n,min,max);
+ShowArray(myArray);
+ int res = FindSum(myArray);
+ Console.WriteLine("Сумма элементов на нечётных позициях равна:"+ res);
